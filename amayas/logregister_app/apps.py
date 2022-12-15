31c19fe498bd989fd39437fb3758759e02cb0903
charishnu <charishnu62@gmail.com>
2022-12-15 18:31:21 +0530

    amayas

---
+++ amayas/logregister_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LogregisterAppConfig(AppConfig):
+    name = 'logregister_app'
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\user\Documents\vs code projects\python3\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
